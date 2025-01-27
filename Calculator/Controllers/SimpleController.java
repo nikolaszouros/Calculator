5fecb26b748226ae66c64c18b57f37925c153d01
I6386110 <n.zouros@student.maastrichtuniversity.nl>
2025-01-27 20:04:28 +0100

    small fixes

--- Calculator/Controllers/SimpleController.java
+++ Calculator/Controllers/SimpleController.java
@@ -7,7 +7,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class SimpleController {
